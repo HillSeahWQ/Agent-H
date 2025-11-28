f1e76d0f335ad9efe5be4851c6c9db4a848c4092
Hill Seah <hill.seah@kyndryl.com>
2025-11-28 17:31:55 +0800

    chunking entry point script

--- vector_db/scripts/run_chunking.py
+++ vector_db/scripts/run_chunking.py
@@ -18,7 +18,7 @@ logger = get_logger(__name__)
 # ======================================================================
 PROJECT_ROOT = Path(__file__).resolve().parent.parent  # AGENT-H/vector_db
 DATA_DIR = PROJECT_ROOT / "data" # create a 'data' folder at AGENT-H/vector_db/data
-INPUT_FOLDER_NAME = "test_pdfs"
+INPUT_FOLDER_NAME = "test_pdfs" # TODO: EDIT THIS, INPUT FOLDER NAME - FOLDER WITH ALL THE DOCUMENTS
 INPUT_DIR = DATA_DIR / INPUT_FOLDER_NAME # AGENT-H/vector_db/data/INPUT_FOLDER_NAME
 CHUNKS_DIR = DATA_DIR / "chunks" # AGENT-H/vector_db/chunks
 CHUNKS_OUTPUT_FILE_NAME = f"{INPUT_FOLDER_NAME}_chunks.json"
